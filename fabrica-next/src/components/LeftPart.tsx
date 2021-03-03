fc5548b9797369311c878d2448b06acfc5ebd2f4
sthephanyel <62970346+sthephanyel@users.noreply.github.com>
2021-03-02 23:28:43 -0300

    alteraçao de design

---
+++ fabrica-next/src/components/LeftPart.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/components/LeftPart.module.css';
+
+export function LeftPart(){
+    return(
+        <div className={styles.leftPartFirst}>
+            <p>teste</p>
+        </div>
+    );
+}
---
+++ fabrica-next/src/components/Upside.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Upside.module.css';
+
+export function Upside(){
+    return(
+        <div className={styles.UpsideFisrt}>
+            <section>
+                <div>
+                    <img src="fabrica704.jpg" alt="fabrica"/>
+                </div>
+                <strong><a href="#">O que é ?</a></strong>
+                <strong>Como Funciona</strong>
+                <strong>Conheça</strong>
+            </section>
+        </div>
+    );
+}
